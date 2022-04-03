334a6cee1a4295f3906206b695ba353e1b885309
bekkahhuss <bekkahhuss@gmail.com>
2022-04-03 11:29:21 -0400

    addded routes to serve HTML files

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 const { notes } = require('./data/notes.json');
 
 
@@ -65,6 +66,12 @@ app.post('/api/notes', (req, res) => {
         const note = createNewNote(req.body, notes);
         res.json(note);
     }
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/notes.html'));
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
